5ba47b9b5d880f75d1cf949ffd2887442c1c482f
tlatorre <tlatorre@uchicago.edu>
2021-09-01 16:10:37 -0700

    update

--- source/qkd_param.cpp
+++ source/qkd_param.cpp
@@ -1,9 +1,7 @@
 #include "qkd_param.h"
 #include "ui_qkd_param.h"
 
-QKD_param::QKD_param(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::QKD_param)
+QKD_param::QKD_param(QWidget *parent) : QWidget(parent), ui(new Ui::QKD_param)
 {
     ui->setupUi(this);
 
@@ -19,9 +17,6 @@ QKD_param::QKD_param(QWidget *parent) :
     QApplication::connect(ui->QKD_iwA, SIGNAL(valueChanged(int)), this, SLOT(slot_QKD_iwA(int)));
     QApplication::connect(ui->QKD_iwB, SIGNAL(valueChanged(int)), this, SLOT(slot_QKD_iwB(int)));
     QApplication::connect(ui->QKD_iwC, SIGNAL(valueChanged(int)), this, SLOT(slot_QKD_iwC(int)));
-    QApplication::connect(ui->QKD_pxqA, SIGNAL(valueChanged(int)), this, SLOT(slot_QKD_pxqA(int)));
-    QApplication::connect(ui->QKD_pxqB, SIGNAL(valueChanged(int)), this, SLOT(slot_QKD_pxqB(int)));
-    QApplication::connect(ui->QKD_pxqC, SIGNAL(valueChanged(int)), this, SLOT(slot_QKD_pxqC(int)));
     QApplication::connect(ui->QKD_zeroA, SIGNAL(valueChanged(int)), this, SLOT(slot_QKD_zeroA(int)));
     QApplication::connect(ui->QKD_zeroB, SIGNAL(valueChanged(int)), this, SLOT(slot_QKD_zeroB(int)));
     QApplication::connect(ui->QKD_zeroC, SIGNAL(valueChanged(int)), this, SLOT(slot_QKD_zeroC(int)));
@@ -33,12 +28,12 @@ QKD_param::QKD_param(QWidget *parent) :
     this->setWindowFlags(Qt::WindowStaysOnTopHint);
 }
 
-void QKD_param::QKD_setDefault(){
+void QKD_param::QKD_setDefault()
+{
     ui->QKD_timeA->setValue(312.5);
     ui->QKD_numbA->setValue(160);
     ui->QKD_phA->setValue(82);
     ui->QKD_iwA->setValue(43);
-    ui->QKD_pxqA->setValue(3);
     ui->QKD_zeroA->setValue(1800);
 
     ui->QKD_timeB->setValue(212.766);
@@ -54,12 +49,8 @@ void QKD_param::QKD_setDefault(){
     ui->QKD_iwB->setValue(44);
     ui->QKD_iwC->setValue(44);
 
-    ui->QKD_pxqB->setValue(3);
-    ui->QKD_pxqC->setValue(3);
-
     ui->QKD_zeroB->setValue(79);
     ui->QKD_zeroC->setValue(79);
-
 }
 
 QKD_param::~QKD_param()
